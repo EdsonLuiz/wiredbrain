b5e23ddde2d4ef3699391c15f84effe10007fda2
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-07-27 16:15:49 -0300

    Make Spring data auto generate findByLastName in FriendService

--- src/main/java/com/wiredbrain/services/FriendService.java
+++ src/main/java/com/wiredbrain/services/FriendService.java
@@ -7,4 +7,5 @@ import com.wiredbrain.models.Friend;
 public interface FriendService extends CrudRepository<Friend, Integer> {
 	Iterable<Friend> findByFirstNameAndLastName(String firstName, String lastName);
 	Iterable<Friend> findByFirstName(String firstName);
+	Iterable<Friend> findByLastName(String lastName);
 }
