da3fb662f70b0328184c5b1254dad80ff9c4ec27
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-07-27 14:20:38 -0300

    Add feature to delete a friend by id on URI /friend/id

--- src/main/java/com/wiredbrain/controllers/FriendController.java
+++ src/main/java/com/wiredbrain/controllers/FriendController.java
@@ -1,7 +1,9 @@
 package com.wiredbrain.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +31,11 @@ public class FriendController {
 	 @PutMapping("/friend")
 	 Friend update(@RequestBody Friend friend) {
 		 return friendService.save(friend);
+	 }
+	 
+	 @DeleteMapping("/friend/{id}")
+	 void delete(@PathVariable Integer id) {
+		 friendService.deleteById(id);
 	 }
 
 }
