ea77efc8e995e256b57d6b3f448a22e1896fc1e1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-07-27 15:22:34 -0300

    Add feature to find friend by id URI /friend/id

--- src/main/java/com/wiredbrain/controllers/FriendController.java
+++ src/main/java/com/wiredbrain/controllers/FriendController.java
@@ -1,5 +1,7 @@
 package com.wiredbrain.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +38,11 @@ public class FriendController {
 	 @DeleteMapping("/friend/{id}")
 	 void delete(@PathVariable Integer id) {
 		 friendService.deleteById(id);
+	 }
+	 
+	 @GetMapping("/friend/{id}")
+	 Optional<Friend> findById(@PathVariable Integer id) {
+		 return friendService.findById(id);
 	 }
 
 }
