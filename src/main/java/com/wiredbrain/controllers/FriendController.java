541ef7e472aca128c3237156389d67d0a1f1e62b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-07-27 14:09:22 -0300

    Add feature to save a single friend on URI /friend

--- src/main/java/com/wiredbrain/controllers/FriendController.java
+++ src/main/java/com/wiredbrain/controllers/FriendController.java
@@ -1,8 +1,11 @@
 package com.wiredbrain.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wiredbrain.models.Friend;
 import com.wiredbrain.services.FriendService;
 
 @RestController
@@ -10,5 +13,10 @@ public class FriendController {
 	
 	 @Autowired
 	 FriendService friendService;
+	 
+	 @PostMapping("/friend")
+	 Friend create (@RequestBody Friend friend) {
+		 return friendService.save(friend);
+	 }
 
 }
