f36a389e473cea09ac3e0349c4aaee1822559cab
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-07-29 13:46:12 -0300

    Make Address an Entity

--- src/main/java/com/wiredbrain/models/Address.java
+++ src/main/java/com/wiredbrain/models/Address.java
@@ -1,9 +1,15 @@
 package com.wiredbrain.models;
 
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Embeddable
+@Entity
 public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String street;
 	private String city;
 
